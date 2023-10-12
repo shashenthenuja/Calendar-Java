caaeb18b0145e298778a416ec22d351c17b340d9
Shashen Thenuja <shashengg@gmail.com>
2023-10-12 14:50:12 +0530

    implemented internationalization

--- app/src/main/java/edu/curtin/assignment2/App.java
+++ app/src/main/java/edu/curtin/assignment2/App.java
@@ -1,54 +1,112 @@
 package edu.curtin.assignment2;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-
 public class App {
+    static String userInput;
+    static ResourceBundle bundle;
+
     public static void main(String[] args) {
         start();
     }
 
     public static void start() {
         Scanner scan = new Scanner(System.in);
+        String lang = chooseLang();
 
         while (true) {
             System.out.print("\033[H\033[2J");
-            cal();
+            cal(lang);
             System.out.print("\n> ");
-            String userInput = scan.next();
+            userInput = scan.next();
         }
     }
 
-    public static void cal() {
-        // Initialising
+    public static void cal(String lang) {
         var terminalGrid = TerminalGrid.create();
 
         var listMessages = new ArrayList<List<String>>();
         var rows = new ArrayList<String>();
         var cols = new ArrayList<String>();
-        var row1 = new ArrayList<String>();
-        var row2 = new ArrayList<String>();
-        var row3 = new ArrayList<String>();
-        rows.add("00:00");
-        rows.add("01:00");
-        rows.add("02:00");
-        cols.add("Oct 11");
-        cols.add("Oct 12");
-        cols.add("Oct 13");
-        row1.add("one");
-        row1.add("two");
-        row1.add("three");
-        row2.add("four");
-        row2.add("five");
-        row2.add("six");
-        row3.add("seven");
-        row3.add("eight");
-        row3.add("nine");
-        listMessages.add(row1);
-        listMessages.add(row2);
-        listMessages.add(row3);
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        Locale locale;
+
+        if (lang.equals("default")) {
+            locale = Locale.US;
+            bundle = ResourceBundle.getBundle("bundle", locale);
+            System.out.println(bundle.getString("ui_chosen_lang") + " : " + locale.getDisplayLanguage() + "\n");
+        } else {
+            try {
+                locale = Locale.forLanguageTag(lang);
+                bundle = ResourceBundle.getBundle("bundle", locale);
+                System.out.println(bundle.getString("ui_chosen_lang") + " : " + locale.getDisplayLanguage() + "\n");
+            } catch (Exception e) {
+                locale = Locale.forLanguageTag(lang);
+                bundle = ResourceBundle.getBundle("bundle", Locale.US);
+                System.out.println(bundle.getString("ui_chosen_lang") + " : " + locale.getDisplayLanguage() + "\n");
+            }
+        }
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy", locale);
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm", locale);
+        // if (userInput.equals("+d") && userInput != null) {
+        // currentDate.plusDays(1);
+        // }
+
+        for (int i = 0; i < 7; i++) {
+            LocalDateTime futureDateTime = currentDateTime.plusDays(i);
+
+            String formattedDate = dateFormat.format(futureDateTime);
+            cols.add(formattedDate);
+        }
+
+        for (int i = 0; i < 24; i++) {
+            LocalTime currentTime = LocalTime.of(i, 0);
+            String formattedTime = timeFormat.format(currentTime);
+            rows.add(formattedTime);
+        }
+        rows.add("All Day \nEvents");
+
+        for (int i = 0; i <= 24; i++) {
+            var row = new ArrayList<String>();
+            for (int j = 0; j < 7; j++) {
+                row.add("-");
+            }
+            listMessages.add(row);
+        }
+
         terminalGrid.print(listMessages, rows, cols);
         System.out.println();
+
+    }
+
+    public static String chooseLang() {
+        Scanner scan = new Scanner(System.in);
+        String lang = "";
+        while (true) {
+            System.out.print("\033[H\033[2J");
+            System.out.println("Choose your language : ");
+            System.out.println("[0] System Default ");
+            System.out.println("[1] Custom Language (Eg. en-US, si-LK) \n");
+            System.out.print("\n> ");
+            String userInput = scan.next();
+            if (userInput.equals("0")) {
+                lang = "default";
+                break;
+            } else if (userInput.equals("1")) {
+                System.out.print("\033[H\033[2J");
+                System.out.print("Enter your language (Eg. en-US, si-LK) :  ");
+                String userLang = scan.next();
+                lang = userLang;
+                break;
+            } else {
+                System.out.println("Enter a valid choice!");
+            }
+        }
+        return lang;
 
     }
 }
